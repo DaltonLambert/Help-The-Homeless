29abf9e9a50442708e3fdde93ceb8a759f0a26a0
Dalton <lambertd757@gmail.com>
2023-02-21 23:16:29 -0500

    Added grid layout and styles for header,main and footer sections for HomePage

--- React/src/components/HomePage.js
+++ React/src/components/HomePage.js
@@ -6,7 +6,8 @@ function HomePage() {
     <div className='homepage-container'>
       {/* header */}
       <header>
-        <h1>HomePage</h1>
+        <img class="logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvectorified.com%2Fimages%2Frelief-icon-33.png&f=1&nofb=1&ipt=5c66ef7aca921c923393178743fa15f370d79bac3902ab0cddb69ea560fea144&ipo=images" alt ="logo" />
+        <h1>Homeless Relief</h1>
         <nav>
           <ul>
             <li><a href="#shelters">Shelters</a></li>
@@ -19,7 +20,7 @@ function HomePage() {
       <main>
         <h2>Welcome to the Homeless Project</h2>
         <p>
-          This is a tool to help connect people experiencing homelessness with resources such as shelter, job training, and mental health services.
+          This is a tool to help connect people experiencing homelessness with resources such as shelters, job training, and mental health services.
         </p>
       </main>
       
